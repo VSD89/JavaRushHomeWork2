6c0b06ed6f17c16eb37115c828374494ab3794b6
Val <vr_89@mail.ru>
2016-08-15 22:31:21 +0400

    Commit

--- src/com/javarush/test/level13/lesson11/bonus02/Person.java
+++ src/com/javarush/test/level13/lesson11/bonus02/Person.java
@@ -1,18 +1,15 @@
 package com.javarush.test.level13.lesson11.bonus02;
 
-public class Person implements RepkaItem
-{
+public class Person implements RepkaItem {
     private String name;
     private String namePadezh;
 
     @Override
-    public String getNamePadezh()
-    {
+    public String getNamePadezh() {
         return namePadezh;
     }
 
-    public Person(String name, String namePadezh)
-    {
+    public Person(String name, String namePadezh) {
         this.name = name;
         this.namePadezh = namePadezh;
     }
