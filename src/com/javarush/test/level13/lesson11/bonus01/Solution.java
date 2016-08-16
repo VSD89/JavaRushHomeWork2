d877a4cc381b3356a6dc663f4a2d2fcccb992b75
Val <vr_89@mail.ru>
2016-08-16 21:17:16 +0400

    Commit

---
+++ src/com/javarush/test/level13/lesson11/bonus01/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.test.level13.lesson11.bonus01;
+
+/* Сортировка четных чисел из файла
+1. Ввести имя файла с консоли.
+2. Прочитать из него набор чисел.
+3. Вывести на консоль только четные, отсортированные по возрастанию.
+Пример ввода:
+5
+8
+11
+3
+2
+10
+Пример вывода:
+2
+8
+10
+*/
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            ArrayList<Integer> num = new ArrayList<Integer>();
+            Scanner sc = new Scanner(new File(reader.readLine()));
+            int data;
+            while(sc.hasNextInt()) { // возвращает истину, если с потока ввода можно считать целое число
+                data = sc.nextInt(); //считывает целое число с потока ввода и сохраняет в переменную
+                if (data%2==0)
+                    num.add(data); //если число четное - добавляем в список
+            }
+            Collections.sort(num);
+            for (int s : num)
+                System.out.println(s);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
