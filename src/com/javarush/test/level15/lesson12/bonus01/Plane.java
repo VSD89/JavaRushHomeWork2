03f7af0e2d0e4a4037cd8ca6d8cca01ad22d8efe
Val <vr_89@mail.ru>
2016-09-07 22:08:01 +0700

    Commit

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+public class Plane implements Flyable {
+    private int numOfPassengers;
+
+    public Plane(int numOfPassengers) {
+        this.numOfPassengers = numOfPassengers;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
