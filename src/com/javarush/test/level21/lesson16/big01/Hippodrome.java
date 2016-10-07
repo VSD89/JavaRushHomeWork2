e8b54c589ea9e805e3ddf9c0406f7a564d1cab1f
Val <vr_89@mail.ru>
2016-10-07 21:52:57 +0700

    Commit

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -1,4 +1,12 @@
 package com.javarush.test.level21.lesson16.big01;
 
+import java.util.ArrayList;
+
 public class Hippodrome {
+
+    ArrayList<Horse> horses = new ArrayList<Horse>();
+
+    public static void main(String[] args) {
+
+    }
 }
