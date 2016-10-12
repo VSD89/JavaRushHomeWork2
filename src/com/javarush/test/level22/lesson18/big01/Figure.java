c7f1d6241e0a9cac9de87b2181486c4fb59e4bd1
Val <vr_89@mail.ru>
2016-10-12 22:53:57 +0700

    Commit

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -1,4 +1,26 @@
 package com.javarush.test.level22.lesson18.big01;
 
 public class Figure {
+
+    public int x;
+    public int y;
+    public int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
 }
