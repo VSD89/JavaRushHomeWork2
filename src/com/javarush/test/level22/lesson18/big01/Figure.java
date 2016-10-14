5960f8a7f0904e28a611b2aee32fb2caa2928de4
Val <vr_89@mail.ru>
2016-10-14 19:01:17 +0700

    Commit

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -24,13 +24,39 @@ public class Figure {
         this.matrix = matrix;
     }
 
-    public void left() {}
-    public void right() {}
-    public void down() {}
-    public void up() {}
+    public void left() {
+        x--;
+    }
+    public void right() {
+        x++;
+    }
+    public void down() {
+        y++;
+    }
+    public void up() {
+        y--;
+    }
     public void downMaximum() {}
     public void rotate() {}
     public boolean isCurrentPositionAvailable() {
+        Field field = Tetris.game.getField();
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (matrix[i][j] == 1)
+                {
+                    if (y + i >= field.getHeight())
+                        return false;
+
+                    Integer value = field.getValue(x + j, y + i);
+                    if (value == null || value == 1)
+                        return false;
+                }
+            }
+        }
+
         return true;
     }
     public void landed() {}
