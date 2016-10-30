72044c41cb4b77e9ab235eae4e559a6a3d00cf0d
Val <vr_89@mail.ru>
2016-10-30 21:47:03 +0700

    Commit

--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -1,8 +1,21 @@
 package com.javarush.test.level24.lesson14.big01;
 
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    public double speed;
+    public double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public Stand(double x, double y) {
+        super(x,y,3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
